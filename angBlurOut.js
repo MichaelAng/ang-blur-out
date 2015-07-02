ed2e44d45c19ef4b583298bbc0b8cce9bb2881e5
Michael Ang <angmerica@gmail.com>
2015-07-02 11:41:28 -0400

    add angBlurOut.js directive.

---
+++ angBlurOut.js
@@ -0,0 +1,69 @@
+(function() {
+angular.module('MyApp', [])
+.directive('angBlurOut', angBlurOut);
+
+function angBlurOut($timeout) {
+  return {
+    link: link,
+    restrict: 'AE',
+    scope: {
+      angCallback: '&',
+      angBlurOut: '=',
+      angInclude: '@?'
+    }
+  };
+
+  function link(scope, elem) {
+    scope.$watch('angBlurOut', function() {
+      if(scope.angBlurOut) {
+        console.log('angBlurOut: ang-blur-out mode is now on.');
+        startEventListening();
+      } else {
+        console.log('angBlurOut: ang-blur-out mode is now off.');
+        stopEventListening();
+      }
+    });
+
+    function clickEvent(event) {
+      scope.angCallback({
+        continuePropagation: function () {
+          $timeout(function () {
+            $(event.target).trigger('click');
+          });
+        }
+      });
+    }
+
+    function startEventListening() {
+      $(document).on('mousedown', clickEvent);
+      elem.on('mousedown', stopPropagation);
+      if (scope.angInclude) {
+        $(scope.angInclude).on('mousedown', stopPropagation);
+      }
+    }
+
+    function stopEventListening() {
+      $(document).off('mousedown', clickEvent);
+      elem.off('mousedown', stopPropagation);
+      if (scope.angInclude) {
+        $(scope.angInclude).off('mousedown', stopPropagation);
+      }
+    }
+
+    function stopPropagation (event) {
+      event.stopPropagation();
+    }
+
+    elem.on("$destroy", function() {
+      console.log('angBlurOut: element destroyed');
+      scope.$destroy();
+    });
+
+    scope.$on("$destroy", function() {
+      console.log('angBlurOut: scope destroyed');
+      stopEventListening();
+    });
+  }
+}
+
+})();
